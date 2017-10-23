89a38af14cfc25af191481b512aeeca096bc0e90
Niklas <niklas-schwerin@t-online.de>
2017-10-23 13:11:27 +0200

    Profile Database query

--- WTS/src/pages/profile/profile.ts
+++ WTS/src/pages/profile/profile.ts
@@ -12,8 +12,19 @@ import firebase from 'firebase';
 
     database:any = firebase.database(); 
     storage:any= firebase.storage(); //file system (Dateien)
+    adresse: any;
     
     constructor(public navCtrl: NavController) {
     }
 
+    ngAfterViewInit(){
+    var userId = '-KwFGCd3o-zKe7UoUKoR';
+    return firebase.database().ref('/Account/' + userId).once('value').then(function(snapshot) {
+      var userObject = snapshot.val();
+      console.log(userObject);
+      
+      this.adresse = userObject.Adresse_id;
+      console.log(this.adresse);
+    });
+  }
   }
