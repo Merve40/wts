97ae56f87109da54bd033cc3ae6ba5e6a07f657d
Niklas <niklas-schwerin@t-online.de>
2017-10-25 11:13:02 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ WTS/src/providers/api/student.ts
@@ -0,0 +1,66 @@
+import { Injectable, Inject } from '@angular/core';
+import { jsonIgnore } from 'json-ignore';
+import { Base } from './base';
+import { Api } from './api';
+import { Table } from './table';
+
+@Injectable()
+export class StudentTable extends Base {
+
+    public Student: {
+        Abschluss: String;
+        Abschluss_Datum: String;
+        Account_id: String;
+        Beschreibung: String;
+        Beschäftigung: String;
+        Geb_Datum: String;
+        Nachname: String;
+        Name: String;
+        Semester: String;
+        Studiengang: String;
+        Uni: String;
+        Vertiefung: String;
+        Zeitstempel: String;
+    };
+
+    constructor( @Inject(Api) public api: Api) {
+        super(Table.STUDENT);
+    }
+
+    getInnerObject(){
+        return this.Student;
+    }
+
+    setInnerObject(o:any){
+        this.Account = o;
+    }
+
+    delete(func:Function):void{
+        this.api.delete(this, func);
+    }
+
+    update<T extends Base>(func:Function){
+        this.api.put(this, func);
+    }
+
+    push<Account>(account: Account, func: Function) {
+        this.api.post(this, account, func);
+    }
+
+    getById(id:string, func:Function){
+        this.api.get(this, id, func);
+    }
+
+    getByValue(key: string, value, func: Function) {
+        this.api.getByValue(this, key, value, func);
+    }
+
+    filterByValue(key: string, value: string, func: Function) {
+        this.api.filterByValue(this, key, value, func);
+    }
+
+    filterByValueAndLimit(key: string, value: string, limit: number, func: Function) {
+        this.api.filterByValueAndLimit(this, key, value, limit, func);
+    }
+}
+
