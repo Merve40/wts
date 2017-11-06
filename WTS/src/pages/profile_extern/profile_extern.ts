1e1d3992349fadb10bb2b6079e0f9ee6d6be8796
jmodinger <32219211+jmodinger@users.noreply.github.com>
2017-11-06 12:08:43 +0100

    gerneral Profile Page NOT working yet
    Page which leads to correct own profile page

--- WTS/src/pages/profile_extern/profile_extern.ts
+++ WTS/src/pages/profile_extern/profile_extern.ts
@@ -1,42 +1,58 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { AccountTable } from '../../providers/api/account';
+import { OnResultComplete } from '../../providers/api/OnResultComplete';
 
-import firebase from 'firebase';
-
-var studentID;
-var AccID = 'acc_10'; // AccountID die wir aus der Suche entnehmen
-var nachname; 
-var vorname;
-var studiengang;
-var uni;
-var adresse;
-
+import { ProfilePage } from '../profile/profile';
+import { CompanyProfilePage } from '../company_profile/company_profile';
+import { UniProfilePage } from '../uni_profile/uni_profile';
 
 @Component({
     selector: 'page-profile_extern',
     templateUrl: 'profile_extern.html'
   })
 
-  export class Profile_externPage{
+  export class Profile_externPage implements OnResultComplete{
 
-    database:any = firebase.database(); 
-    storage:any = firebase.storage(); //file system (Dateien)
+    accID = 'acc_11';    
     
-    constructor(public navCtrl: NavController) {
+    constructor(public storage:Storage, public navCtrl: NavController, public navParams: NavParams,
+      public AccountTable: AccountTable) {
+
+        console.log("Started constructor");
+        //this.accID = navParams.get("userId");
+        this.AccountTable.getById(this.accID, "account-abfrage", this.onComplete);
+        }
+
+    onComplete(src, json) {
+      if (src == "account-abfrage") {
+        console.log("went into onComplete");
+        var body = json.body;
+        var group_id = body.Usergruppe;
+        console.log("group_id: " + group_id);        
+        this.navigateToUserProfile(json);
+        console.log("END");     
+      } 
+      console.log("Printed as well");   
     }
 
-  ngAfterViewInit(){
-    this.database.ref('/Student/')
-    .orderByChild('Account_Id').equalTo(AccID)
-    .on('value', function (snapshot) {
-      snapshot.forEach(element => {
-        nachname = (element.val().Nachname);
-        vorname = (element.val().Name);
-        studiengang = (element.val().Studiengang);
-        uni = (element.val().Uni);
-        console.log(nachname);
-        console.log(vorname); 
-      });
-    });    
+    navigateToUserProfile(json) {
+      console.log("went into navigateToUserProfile");
+      //this.storage.set("user_id", json.id);
+       
+      switch (json.body.Usergruppe) {
+        case "gruppe_1": this.navCtrl.push(ProfilePage, { userId: json.id });
+          break;
+        //todo: Student Profil (ProfilePage) mit Unternehmen Profil ersetzen
+        case "gruppe_2": this.navCtrl.push(CompanyProfilePage, { userId: json.id });
+          break;
+        //todo: Student Profil (ProfilePage) mit Uni Profil ersetzen
+        case "gruppe_3": this.navCtrl.push(UniProfilePage, { userId: json.id });
+          break;
+      }
+    }
+
+  ngAfterViewInit(){   
   }
-  }
+}
