663a73c7179b43bc9d107099aa36e065e1947cf7
Merve <merve_sahin_94@hotmail.de>
2017-11-09 14:20:41 +0100

    GUI für Nachrichtenliste implementiert

--- WTS/src/pages/message/message_list/message_list.ts
+++ WTS/src/pages/message/message_list/message_list.ts
@@ -11,14 +11,22 @@ export class MessageListPage {
 
     //Testdaten Nachtichtenliste
     messages:any = [
-        {userName:"John Doe", img:"https://goo.gl/images/MyjyYu", lastMessage:"hello", dateTime:"20:49"},
-        {userName:"Max Mustermann", img:"https://goo.gl/images/MyjyYu", lastMessage:"test", dateTime:"19:54"},
-        {userName:"Michael Scott", img:"https://goo.gl/images/MyjyYu", lastMessage:"goodbye", dateTime:"07.11.2017"},
-        {userName:"Dwight Schrute", img:"https://goo.gl/images/MyjyYu", lastMessage:"how are you?", dateTime:"06.11.2017"},
-        {userName:"Jim Halpter", img:"https://goo.gl/images/MyjyYu", lastMessage:"see u later", dateTime:"17.10.2017"},
-        {userName:"Pam Beesly", img:"https://goo.gl/images/MyjyYu", lastMessage:"by", dateTime:"01.10.2017"}
+        {id: 0, userName:"John Doe", img:"https://goo.gl/images/MyjyYu", lastMessage:"hello", dateTime:"20:49"},
+        {id: 1, userName:"Max Mustermann", img:"https://goo.gl/images/MyjyYu", lastMessage:"test", dateTime:"19:54"},
+        {id: 2, userName:"Michael Scott", img:"https://goo.gl/images/MyjyYu", lastMessage:"goodbye", dateTime:"07.11.2017"},
+        {id: 3, userName:"Dwight Schrute", img:"https://goo.gl/images/MyjyYu", lastMessage:"how are you?", dateTime:"06.11.2017"},
+        {id: 4, userName:"Jim Halpter", img:"https://goo.gl/images/MyjyYu", lastMessage:"see u later", dateTime:"17.10.2017"},
+        {id: 5, userName:"Pam Beesly", img:"https://goo.gl/images/MyjyYu", lastMessage:"by", dateTime:"01.10.2017"}
     ];
 
     constructor(public navCtrl: NavController, public navparams: NavParams, public translate: TranslateService) {
+    }
+
+    openMessage(id){
+        console.log(id);
+    }
+
+    ngAfterViewInit(){
+        //TODO: load list
     }
 }
