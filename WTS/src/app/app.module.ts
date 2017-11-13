07ebc8224779edf78e75598879b9570dbfa9aa9d
Niklas <niklas-schwerin@t-online.de>
2017-11-13 09:45:05 +0100

    Hallo Moritz

--- WTS/src/app/app.module.ts
+++ WTS/src/app/app.module.ts
@@ -50,6 +50,7 @@ import { MockProvider } from '../pages/list_search/dataprovider';
     Profile_EditPage,
     ListSearchPage,
     MapPage,
+    ContactRequestPage,
     CompanyProfilePage,
     MessagePage,
     MessageListPage,
