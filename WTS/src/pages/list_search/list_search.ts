e6ec06162809e19cdb78d944f93cc1cbd93ba243
Moritz Richter <moritzrichter96@t-online.de>
2017-11-21 11:19:48 +0100

    list search lazy loading

--- WTS/src/pages/list_search/list_search.ts
+++ WTS/src/pages/list_search/list_search.ts
@@ -14,10 +14,11 @@ export class ListSearchPage implements OnResultComplete {
 
   result = [];
   filter = "";
-  pagesize = 10;
+  pagesize = 4;
   searchObject = "Student";
   searchParameterStudent = "Name";
   searchParameterUniversity = "Universität";
+  infinityScroll;
 
   constructor(public navCtrl: NavController, public StudentTable: StudentTable, public UniversityTable: UniversityTable) {
     StudentTable.setSrcClass(this);
@@ -25,23 +26,43 @@ export class ListSearchPage implements OnResultComplete {
   }
 
   onComplete(src, json) {
-    var found = false;
-    console.log(json);
     switch (src) {
-      case "student-search-query": {
+      case "first-name-search-query": {
         if (this.result.length < this.pagesize) {
           json.forEach(element => {
+            var found = false;
             var body = element.body;
-            found = false;
             for (var i = 0; i < this.result.length; i++) {
-              var id = this.result[i].id
-              if (id == body.id) {
+              var id = this.result[i].id;
+              if (id == body.Account_Id) {
                 found = true;
                 break;
               }
             }
             if (this.result.indexOf(body) < 0 && this.result.length < this.pagesize && !found) {
-              this.result.push(new User(body.Account_Id, body.Name + " " + body.Nachname, body.Uni));
+              var user = new User(body.Account_Id, body.Name + " " + body.Nachname, body.Uni);
+              this.result.push(user);
+            }
+          });
+        }
+        this.StudentTable.getAllContaining("Nachname", this.filter, "student-search-query", this.onComplete);
+        return;
+      }
+      case "student-search-query": {
+        if (this.result.length < this.pagesize) {
+          json.forEach(element => {
+            var found = false;
+            var body = element.body;
+            for (var i = 0; i < this.result.length; i++) {
+              var id = this.result[i].id;
+              if (id == body.Account_Id) {
+                found = true;
+                break;
+              }
+            }
+            if (this.result.indexOf(body) < 0 && this.result.length < this.pagesize && !found) {
+              var user = new User(body.Account_Id, body.Name + " " + body.Nachname, body.Uni);
+              this.result.push(user);
             }
           });
         }
@@ -50,31 +71,49 @@ export class ListSearchPage implements OnResultComplete {
       case "university-search-query": {
         if (this.result.length < this.pagesize) {
           json.forEach(element => {
+            var found = false;
             var body = element.body;
-            found = false;
             for (var i = 0; i < this.result.length; i++) {
-              var id = this.result[i].id
-              if (id == body.id) {
+              var id = this.result[i].id;
+              if (id == body.Account_Id) {
                 found = true;
                 break;
               }
             }
             if (this.result.indexOf(body) < 0 && this.result.length < this.pagesize && !found) {
-              this.result.push(new User(body.Account_Id, body.Universität, body.Fachrichtungen));
+              var user = new User(body.Account_Id, body.Universität, body.Fachrichtungen);
+              this.result.push(user);
             }
           });
         }
+        break;
       }
       default: {
         break;
       }
     }
+    if (this.infinityScroll != null) {
+      this.infinityScroll.complete();
+      if (this.result.length < this.pagesize) {
+        this.infinityScroll.enabled = false;
+      }
+    }
+
   }
 
   search() {
-    if(this.filter == ""){
-      return;
+    if (this.infinityScroll != null) {
+      this.infinityScroll.enabled = true;
+      this.infinityScroll = null;
     }
+    this.pagesize = 4;
+    this.result = [];
+    if (this.filter != "") {
+      this.searchForUser();
+    }
+  }
+
+  searchForUser() {
     if (this.searchObject == 'Student') {
       this.searchForStudents();
     }
@@ -85,48 +124,51 @@ export class ListSearchPage implements OnResultComplete {
   }
 
   searchForStudents() {
-    this.result = [];
     if (this.searchParameterStudent.length > 0 && this.searchParameterStudent != "Name") {
       this.StudentTable.getAllContaining(this.searchParameterStudent, this.filter, "student-search-query", this.onComplete);
     } else if (this.searchParameterStudent.length > 0) {
       if (this.filter.indexOf(" ") !== -1) {
         var paras = this.filter.split(" ");
         paras.forEach(element => {
-          this.StudentTable.getAllContaining("Nachname", this.filter, "student-search-query", this.onComplete);
-          this.StudentTable.getAllContaining("Name", this.filter, "student-search-query", this.onComplete);
+          this.StudentTable.getAllContaining("Name", this.filter, "first-name-search-query", this.onComplete);
         });
       } else {
-        this.StudentTable.getAllContaining("Nachname", this.filter, "student-search-query", this.onComplete);
-        this.StudentTable.getAllContaining("Name", this.filter, "student-search-query", this.onComplete);
+        this.StudentTable.getAllContaining("Name", this.filter, "first-name-search-query", this.onComplete);
       }
     }
-
   }
 
   searchForUniversity() {
-    this.result = [];
-    console.log(this.filter);
     if (this.searchParameterUniversity.length > 0) {
       this.UniversityTable.getAllContaining(this.searchParameterUniversity, this.filter, "university-search-query", this.onComplete);
     }
   }
 
   navigateToUserProfile(id) {
-    console.log(id);
     this.navCtrl.setRoot(ProfileVarier, { userId: id });
   }
 
   ngAfterViewInit() {
+  }
+
+  loadMore(infinityScroll) {
+    this.infinityScroll = infinityScroll;
+    if (this.pagesize == this.result.length) {
+      this.pagesize += 1;
+      this.searchForUser();
+    }else{
+      this.infinityScroll.enabled = false;
+    }
   }
 }
 
 class User {
   id: string;
   name: string;
-  discription: string;
-  constructor(id: string, name: string, discription: string) {
+  description: string;
+  constructor(id: string, name: string, description: string) {
     this.id = id;
     this.name = name;
-    this.discription = discription;
+    this.description = description;
   }
 }
