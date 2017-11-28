bc109630fec8d3eccb3e5889dda7df31aa16afe4
Moritz Richter <moritzrichter96@t-online.de>
2017-11-28 09:57:17 +0100

    added student tab in network

---
+++ WTS/src/pages/network/tabs/students/students.ts
@@ -0,0 +1,17 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController } from 'ionic-angular/navigation/nav-controller';
+
+@Component({
+    selector: 'page-network',
+    templateUrl: 'network.html'
+  })
+export class StudentNetwork{
+
+  constructor(public navCtrl:NavController){    
+  }
+
+  ngAfterViewInit(){
+  }
+
+  
+}
