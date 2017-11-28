4078c8da9f122bbc34385b3c21f300a98e83bdf7
Moritz Richter <moritzrichter96@t-online.de>
2017-11-28 11:39:20 +0100

    merge

--- WTS/src/pages/network/tabs/students/students.ts
+++ WTS/src/pages/network/tabs/students/students.ts
@@ -1,5 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular/navigation/nav-controller';
+import { AccountTable } from '../../../../providers/api/account';
+import { ContactRequestTable } from '../../../../providers/api/contactrequest';
+import { StudentTable } from '../../../../providers/api/student';
 import { DataProvider } from '../DataProvider';
 
 @Component({
@@ -8,15 +11,51 @@ import { DataProvider } from '../DataProvider';
   })
 export class StudentNetwork{
 
-  result:any[];
+    students = [];
 
-  constructor(public navCtrl:NavController){    
+    constructor(public storage: Storage, public navCtrl: NavController, public ContactRequestTable: ContactRequestTable, public StudentTable: StudentTable) {
+        ContactRequestTable.setSrcClass(this);
+        StudentTable.setSrcClass(this);
     var provider =  new DataProvider();
-    this.result = provider.getData();
-  }
+    this.students = provider.getData();
+    }
 
-  ngAfterViewInit(){
-  }
+    onComplete(src, json) {
+        switch (src) {
+            case "contact-query":
+                for (var i = 0; i < json.length; i++) {
+                    if (json[i].body == null) {
+                        break;
+                    }
+                    else {
+                        var sender = json[i].body.sender;
+                        var receiver = json[i].body.receiver;
+                        var request = json[i].body.request;
+                        if (request == true) {
+                            this.StudentTable.getByValue("Account_Id", sender, "account-request", this.onComplete);
+                            this.StudentTable.getByValue("Account_Id", receiver, "account-request", this.onComplete);
+                        }
+                    }
+                };
+                break;
+
+            case "account-request":
+                this.students.push(new User(json.body.Account_Id, json.body.Name + " " + json.body.Nachname, json.body.Uni));
+                console.log(this.students);
+                break;
+        }
+    }
+
+    ngAfterViewInit() {
+        console.log("test");
+        this.storage.get("user_id").then((id) => this.searchForContacts(id));
+    }
+
+    searchForContacts(id) {
+        this.ContactRequestTable.getAllContaining("reciever", id, "contact-query", this.onComplete);
+        this.ContactRequestTable.getAllContaining("sender", id, "contact-query", this.onComplete);
+    }
+
 
   loadMore(event:any){
     console.log("loading..");
