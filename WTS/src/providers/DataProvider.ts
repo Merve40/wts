cd3dc9ee31bddce870dda381744f1f2b5708d83b
Merve <merve_sahin_94@hotmail.de>
2017-11-28 13:22:18 +0100

    cleanup + fix

--- WTS/src/providers/DataProvider.ts
+++ WTS/src/providers/DataProvider.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild , Injectable } from '@angular/core';
-import { NavController } from 'ionic-angular/navigation/nav-controller';
+import { Component, ViewChild, Injectable } from '@angular/core';
 import { AccountTable } from '../providers/api/account';
 import { ContactRequestTable } from '../providers/api/contactrequest';
 import { StudentTable } from '../providers/api/student';
@@ -9,12 +8,11 @@ import { OnResultComplete } from '../providers/api/OnResultComplete';
  * Service Class for getting Network information
  */
 @Injectable()
-export class DataProvider implements OnResultComplete{
-
+export class DataProvider implements OnResultComplete {
 
     students = [];
 
-    constructor(public storage: Storage, public navCtrl: NavController, public ContactRequestTable: ContactRequestTable, public StudentTable: StudentTable){
+    constructor(public storage: Storage, public ContactRequestTable: ContactRequestTable, public StudentTable: StudentTable) {
         ContactRequestTable.setSrcClass(this);
         StudentTable.setSrcClass(this);
         console.log("start");
@@ -46,18 +44,17 @@ export class DataProvider implements OnResultComplete{
         }
     }
 
-
     searchForContacts(id) {
         this.ContactRequestTable.getAllContaining("receiver", id, "contact-query", this.onComplete);
         this.ContactRequestTable.getAllContaining("sender", id, "contact-query", this.onComplete);
     }
 
 
-  loadMore(event:any){
-    console.log("loading..");
-  }
+    loadMore(event: any) {
+        console.log("loading..");
+    }
 
-    public getStudents(){
+    public getStudents() {
         return this.students;
     }
 
