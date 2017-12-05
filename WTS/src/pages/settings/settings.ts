8b7cd03ffaa690887fb9819435df9105c6d3ca30
Merve <merve_sahin_94@hotmail.de>
2017-12-05 14:08:30 +0100

    Settings speichern implementiert

--- WTS/src/pages/settings/settings.ts
+++ WTS/src/pages/settings/settings.ts
@@ -6,6 +6,7 @@ import { VisibilityTable } from '../../providers/api/visibility';
 import * as moment from 'moment';
 import { OnResultComplete } from '../../providers/api/OnResultComplete';
 import { BlockTable } from '../../providers/api/block';
+import { Storage } from '@ionic/storage';
 
 
 /**
@@ -21,17 +22,32 @@ export class Settings implements OnResultComplete{
   visibilityBlocks = [];
 
   accID: string; // AccountID die wir aus dem Login entnehmen
-  personalInfo:boolean;
-  universityInfo:boolean;
-  emailInfo:boolean;
+
+  personaltoggle = {
+    toggle: true,
+    block: undefined
+  }
+  studytoggle= {
+    toggle: true,
+    block:undefined
+  }
+  emailtoggle= {
+    toggle: true,
+    block: undefined
+  }
+  addresstoggle= {
+    toggle: true,
+    block: undefined
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public translate: TranslateService,
-            public toastCtrl: ToastController, public blockTable:BlockTable, public visibility:VisibilityTable) {
+            public toastCtrl: ToastController, public blockTable:BlockTable, public visibility:VisibilityTable,
+          public storage:Storage) {
 
     this.blockTable.setSrcClass(this);
     this.visibility.setSrcClass(this);
-    this.accID = navParams.get("userId");
-    this.loadData();
+    this.storage.get("user_id").then( id => this.loadData(id) );
+    
   }
 
   /**
@@ -44,33 +60,53 @@ export class Settings implements OnResultComplete{
   }
 
   discardChanges() {
-    this.loadData();
+    this.loadData(this.accID);
+  }
+
+  /**
+   * Changes the visibility of the specific setting
+   * @param togglebutton button that is being clicked / toggled
+   */
+  changeSetting(togglebutton){
+    togglebutton.block.body.Sichtbar = togglebutton.toggle;
+    this.visibility.update(togglebutton.block.id, togglebutton.block.body, "", this.onComplete);
   }
 
   onComplete(src, json) {
     //Auslesen der Daten aus Tabelle Student where AccID = AccID
 
     if(src == "data"){
+      console.log(json);
       if(json[0].body != null){
         //loads all visibility settings by the current users account id
-        for(let obj of json){
+       for(let obj of json){
           this.visibilityBlocks.push(obj);
 
           //iterates through the blocks to find the specific block and set it's boolean value 
           for(let block of this.blocks){
 
             if(obj.body.Block_Id == block.id){
+            
               if(block.body.Block_Name == "Persönlich"){
-                this.personalInfo = obj.body.Sichtbar;
+                this.personaltoggle.toggle = obj.body.Sichtbar;
+                this.personaltoggle.block = obj;
                 break;
 
-              }else if(obj.body.Block_Name == "Email"){
-                this.emailInfo = obj.body.Sichtbar;
+              }else if(block.body.Block_Name == "Email"){
+                this.emailtoggle.toggle = obj.body.Sichtbar;
+                this.emailtoggle.block = obj;
                 break;
 
-              }else if(obj.body.Block_Name == "Studium"){
-                this.universityInfo = obj.body.Sichtbar;
+              }else if(block.body.Block_Name == "Studium"){
+                this.studytoggle.toggle = obj.body.Sichtbar;
+                this.studytoggle.block = obj;
                 break;
+
+              }else if (block.body.Block_Name == "Adresse"){
+                this.addresstoggle.toggle = obj.body.Sichtbar;
+                this.addresstoggle.block = obj;
+                break;
+
               }
             }
           }
@@ -81,17 +117,21 @@ export class Settings implements OnResultComplete{
 
   }
 
-  loadData() {
-    var blockNames = ["Persönlich", "Email", "Studium"];
+  loadData(id) {
+    console.log("loading data..");
+    this.accID = id;
+    var blockNames = ["Persönlich", "Email", "Studium", "Adresse"];
 
     for(var i = 0; i < blockNames.length; i++){
       this.blockTable.getByValue("Block_Name", blockNames[i], ""+i, (src, json)=>{
+
         this.blocks.push(json);
         var index = +src;
         //if the last entry was fetched then do the next request
-        if(index+1 == this.blocks.length){
+        if(index+1 == blockNames.length){
           this.visibility.filterByValue("Account_Id", this.accID, "data", this.onComplete);
         }
+
       });
     }
     
