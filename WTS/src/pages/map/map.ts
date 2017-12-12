18562797ff44095373dae558119e348e846dfc06
Moritz Richter <moritzrichter96@t-online.de>
2017-12-12 13:14:45 +0100

    map set pin

--- WTS/src/pages/map/map.ts
+++ WTS/src/pages/map/map.ts
@@ -8,6 +8,7 @@ import { SkillTable } from '../../providers/api/skill';
 import { Student_PassionTable } from '../../providers/api/student_passion';
 import { PassionTable } from '../../providers/api/passion';
 import { OnResultComplete } from '../../providers/api/OnResultComplete';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-map',
@@ -16,7 +17,7 @@ import { OnResultComplete } from '../../providers/api/OnResultComplete';
 export class MapPage implements OnResultComplete {
 
 
-  constructor(public navCtrl: NavController, public AdressTable: AdressTable, public StudentTable: StudentTable, public AccountTable: AccountTable, public StudentSkillTable: Student_SkillTable, public SkillTable: SkillTable, public PassionTable: PassionTable, public StudentPassionTable: Student_PassionTable) {
+  constructor(public navCtrl: NavController, public AdressTable: AdressTable, public StudentTable: StudentTable, public AccountTable: AccountTable, public StudentSkillTable: Student_SkillTable, public SkillTable: SkillTable, public PassionTable: PassionTable, public StudentPassionTable: Student_PassionTable, public httpClient: HttpClient) {
     StudentTable.setSrcClass(this);
     AccountTable.setSrcClass(this);
     AdressTable.setSrcClass(this);
@@ -30,6 +31,27 @@ export class MapPage implements OnResultComplete {
   }
 
   ngAfterViewInit() {
+  }
+
+  openSetPinPage() {
+
+    var url = "http://www.ynfynyty.net/mapapp/api/create.aspx?mapid=509&pwc=hg67UH!&backwardurl=www.testing.worktostudent.com";
+
+    try {
+      var info = this.httpClient.get(url);
+      info
+        .subscribe(data => {
+          console.log('my data: ', data);
+        },error =>{
+          console.log('my error: ', error)
+        })
+    }
+    catch (err) {
+      console.log("error");
+      document.getElementById("demo").innerHTML = err.message;
+      //console.log('my error: ', err);
+    }
+
   }
 }
 
